97dd3d18f252f771c22d82957c1c7a3f0e5c8272
NIGHTMAReS <midogames331@gmail.com>
2025-05-07 00:06:55 +0300

    Update 1.js

--- stories/1/1.js
+++ stories/1/1.js
@@ -45,8 +45,9 @@ window.onscroll = function () {
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // لضمان التمرير للأعلى عند الوصول للأعلى
   });
 
+// دالة لتبديل إظهار الأزرار عند النقر على زر المشاركة
 function toggleShareButtons() {
-  const buttons = document.getElementById('additional-buttons');
-  buttons.style.display = buttons.style.display === 'none' || buttons.style.display === '' ? 'block' : 'none';
+  var buttons = document.getElementById('additional-buttons');
+  buttons.classList.toggle('show');  // التبديل بين الإظهار والإخفاء
 }
-  
+
