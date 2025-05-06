0f90eb4a489e9152b5654fe0c969517782d227b6
NIGHTMAReS <midogames331@gmail.com>
2025-05-06 23:13:33 +0300

    Add files via upload

---
+++ stories/3/3.js
@@ -0,0 +1,24 @@
+// شريط التمرير العلوي
+window.addEventListener('scroll', () => {
+  const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const progress = (window.scrollY / scrollHeight) * 100;
+  document.getElementById('progress-bar').style.width = progress + '%';
+});
+
+
+let lastScrollTop = 0;
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    // عند النزول - اخفِ الشريط بتحريك للأعلى
+    navbar.style.transform = 'translate(-50%, -100px)';
+  } else {
+    // عند الصعود - أعده لمكانه
+    navbar.style.transform = 'translate(-50%, 0)';
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
