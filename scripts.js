4525a9b2fd793c282edc2bc3a5d76a088a7eb70a
NIGHTMAReS <midogames331@gmail.com>
2025-07-06 17:16:37 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -141,4 +141,21 @@ db.ref("news").once("value").then(snapshot => {
 
 
 
+  const notifBtn = document.getElementById("notifBtn");
+  const notifMenu = document.getElementById("notifMenu");
+  const notifDot = document.getElementById("notifDot");
+  const notifList = document.getElementById("notificationsList");
+
+  // فتح/إغلاق القائمة
+  notifBtn.addEventListener("click", () => {
+    notifMenu.classList.toggle("hidden");
+    notifDot.classList.add("hidden"); // إخفاء النقطة الحمراء عند فتح القائمة
+  });
+
+  // غلق القائمة عند الضغط خارجها
+  window.addEventListener("click", (e) => {
+    if (!notifBtn.contains(e.target) && !notifMenu.contains(e.target)) {
+      notifMenu.classList.add("hidden");
+    }
+  });
 
